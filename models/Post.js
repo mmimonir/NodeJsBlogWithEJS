56fb0900daa2a54c6bdc7d15da2d129aa68a961c
mmimonir <monirdhk.it@gmail.com>
2019-08-05 01:32:05 +0600

    Create Router

---
+++ models/Post.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose')
+const User = require('./User')
+const Comment = require('./Comment')
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 100
+    },
+    body: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: User,
+      requried: true
+    },
+    tags: {
+      type: [String],
+      required: true
+    },
+    thumbnail: String,
+    readTime: String,
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: User
+      }
+    ],
+    dislike: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: User
+      }
+    ],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: Comment
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Post = model('Post', postSchema)
+module.exports = Post
